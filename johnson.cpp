85b99bed52220de7fec22ca769a14df795b0677e
Stan <sroma@users.noreply.github.com>
2015-04-17 06:59:40 +0900

    Update johnson.cpp

--- johnson.cpp
+++ johnson.cpp
@@ -4,112 +4,110 @@
 #include <string>
 #include <sstream>
 #include <vector>
+#include <set>
 #include <omp.h>
-
 using namespace std;
 
+struct Edge {
+    int start;
+    int end;
+    int weight;
+};
 struct Graph {
-    int ** A;
+    Edge * E;
     int VortexCount;
     int EdgeCount;
 };
 
-// Floyd-Warshall
-int FloydWarshall(int ** D, int ** P, int N) {
-    for (int k = 0; k < N; k++)
-        #pragma omp parallel for
-        for (int i = 0; i < N; i++)
-            for (int j = 0; j < N; j++) 
-                if (D[i][k] < INT_MAX && D[k][j] < INT_MAX) {
-                    int d = D[i][k] + D[k][j];
-                    if (d < D[i][j]) {
-                         D[i][j] = d;
-                         P[i][j] = P[k][j];
-                    }
-                }
-    return 0;
+void ConstructPath(int j, int * p, vector<int>& path) {
+    path.clear();
+    if (p[j] == INT_MAX) path.push_back(-INT_MAX);
+    else 
+        for (int i = j; i != -1; i = p[i])
+            path.push_back(i);
 }
 
-// get path
-void ConstructPath(int ** P, int i, int j, vector<int>& path) {
-    if (i == j) {
-        path.clear();
-        path.push_back(i);
+void PrintPath(Graph G, int i, int j, int d, int * p) {
+    vector<int> path;
+    cout << "Path " << i+1 << "-" << j+1 << ", d = " << d << endl;
+    ConstructPath(j, p, path);    
+    reverse(path.begin(), path.end());
+    for (int i = 0; i < path.size(); i++) cout << path[i] + 1 << "-";
+    cout << endl;
+    int s = 0;
+    for (int i = 0; i < path.size() - 1; i++)
+	for (int j = 0; j < G.EdgeCount; j++) 
+	    if (G.E[j].start == path[i] && G.E[j].end == path[i+1]) {
+    		cout << G.E[j].weight << "+";
+    		s +=  G.E[j].weight;
+    		break;
+    	    }
+    cout << " = " << s << endl;
+}
+
+// Bellman-Ford algorithm, s - source of search
+// d - length from k to other vertexes
+bool BellmanFord(Graph G, int s, int * d, int * p) {
+    #pragma omp parallel for
+    for (int i = 0; i < G.VortexCount; i++) {
+        d[i] = INT_MAX;
+        p[i] = -1;
     }
-    else if (P[i][j] == -INT_MAX) {
-        path.clear();
-        path.push_back(-INT_MAX);
-        } else {
-            ConstructPath(P, i, P[i][j], path); 
-            path.push_back(j);
+    d[s] = 0;
+    for (int i = 0; i <= G.VortexCount; i++) // last iter check negative cycle
+	    for (int j = 0; j < G.EdgeCount; j++) {
+	        if (d[G.E[j].start] < INT_MAX && d[G.E[j].end] > d[G.E[j].start] + G.E[j].weight) {
+		        if (i == G.VortexCount) return true;
+                d[G.E[j].end] = d[G.E[j].start] + G.E[j].weight;
+                p[G.E[j].end] = G.E[j].start;
+            }    
         }
+    return false;
 }
 
-void ConstructPath6(int P[6][6], int i, int j) {
-    if (i == j) cout << i+1 << " ";
-    else if (P[i][j] == -INT_MAX)
-        cout << i+1 << " - " << j+1;
-    else {
-        ConstructPath6(P, i, P[i][j]); 
-        cout << j+1 << " ";
+// Dijkstra alg (marks and distances store in arrays)
+void Dijkstra(Graph G, int s, int *d, int * p) {
+    bool * u = new bool[G.VortexCount];
+    #pragma omp parallel for
+    for (int i = 0; i < G.VortexCount; i++) {
+        d[i] = INT_MAX;
+        p[i] = -1;
+	    u[i] = false;
+    }    
+    d[s] = 0;
+    
+    for (int i = 0; i < G.VortexCount; i++) {
+	    int mind = INT_MAX;
+	    int cur = INT_MAX;
+	    for (int j = 0; j < G.VortexCount; j++) 
+	        if (d[j] < mind && !u[j]) {
+		        mind = d[j];
+		        cur = j;
+	        }
+	
+	    if (mind == INT_MAX || cur == INT_MAX) break; // graph is not connected
+	    
+	    u[cur] = true;
+	    for (int j = 0; j < G.EdgeCount; j++) {
+	        if (G.E[j].start == cur) {
+		        int dist = d[cur] + G.E[j].weight;
+		        if (d[G.E[j].end] > dist) {
+		            d[G.E[j].end] = dist;
+		            p[G.E[j].end] = cur;
+		        }
+	        }
+	    }
     }
 }
 
 int main(int argc, char* argv[]) {
-    /* int A6[6][6] = {{0,10,20,30,0,0},
-                      {0,0,0,0,0,7},
-                      {0,0,0,0,0,5},
-                      {0,0,0,0,10,0},
-                      {2,0,0,0,0,4},
-                      {0,5,7,0,6,0}};
-    int D6[6][6], P6[6][6];
-     cout << "Test case" << endl;
-    // Floyd-Warshall (test case)
-    for (int i = 0; i < 6; i++)
-        for (int j = 0; j < 6; j++) {
-            D6[i][j] = A6[i][j];
-            P6[i][j] = i;
-            if (i != j && D6[i][j] == 0) {
-                D6[i][j] = INT_MAX;
-                P6[i][j] = -INT_MAX;
-            }
-        }
-    for (int k = 0; k < 6; k++)
-        for (int i = 0; i < 6; i++)
-            for (int j = 0; j < 6; j++) 
-                if (D6[i][k] < INT_MAX && D6[k][j] < INT_MAX) {
-                    int d = D6[i][k] + D6[k][j];
-                    if (d < D6[i][j]) {
-                         D6[i][j] = d;
-                         P6[i][j] = P6[k][j];
-                    }
-                }
-
-    cout << "Distance matrix" << endl;
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) 
-            if (D6[i][j] < INT_MAX) cout << D6[i][j] << " ";
-            else cout << "inf ";
-        cout << endl;
-    }
-    cout << "Predessesor matrix" << endl;
-    for (int i = 0; i < 6; i++) {
-        for (int j = 0; j < 6; j++) 
-            cout << P6[i][j] << " ";
-        cout << endl;
-    }
-    cout << "Path 1-5, d = " << D6[0][4] << ": ";
-    ConstructPath6(P6,0,4); 
-    */
     int num_threads = (argc > 1) ? atoi(argv[1]) : 1; 
     omp_set_num_threads(num_threads);
 
-    int **P, **D;
     Graph G;
-    bool declared = false;
+    int num = 0;
     string GraphType, token;
-    vector<int> path; 
-
+    bool declared = false;
     cout << endl << "Roma graph" << endl;
     ifstream fl("./roma99.gr");
     for(string line; getline(fl, line);) {
@@ -117,76 +115,69 @@ int main(int argc, char* argv[]) {
         if (line[0] == 'p') {
             istringstream iss(line);
             iss >> token >> GraphType >> G.VortexCount >> G.EdgeCount;
-
-            if (G.VortexCount > 0) {
-                D = new int*[G.VortexCount];
-                P = new int*[G.VortexCount];
-                G.A = new int*[G.VortexCount];
-                for (int i = 0; i < G.VortexCount; i++) {
-                    D[i] = new int[G.VortexCount];
-                    P[i] = new int[G.VortexCount];
-                    G.A[i] = new int[G.VortexCount];
-                }
-                #pragma omp parallel for
-                for (int i = 0; i < G.VortexCount; i++)
-                    for (int j = 0; j < G.VortexCount; j++) G.A[i][j] = 0;
-                declared = true;
-            }
-        } // p
+	    
+	        if (G.VortexCount > 0 && G.EdgeCount > 0) {
+		        G.E = new Edge[G.EdgeCount];
+	    	    declared = true;
+	        }
+	    
+        } 
         if (declared && line[0] == 'a') {
             int i, j, w;
             istringstream iss(line);
             iss >> token >> i >> j >> w; 
-            G.A[i-1][j-1] = w;     
+            G.E[num].start = i-1;
+	        G.E[num].end = j-1;
+	        G.E[num].weight = w;
+	        num++;     
         }
-    }    
-    if (!declared) return 1;
-
-     # pragma omp parallel for
-     for (int i = 0; i < G.VortexCount; i++)
-        for (int j = 0; j < G.VortexCount; j++) {
-            D[i][j] = G.A[i][j];
-            P[i][j] = i;
-            if (i != j && D[i][j] == 0) {
-                P[i][j] = -INT_MAX;
-                D[i][j] = INT_MAX;
-            }
-        }
+    } 
+          
     double tm = omp_get_wtime(); 
-    FloydWarshall(D, P, G.VortexCount);
-    tm = omp_get_wtime() - tm; 
-    cout << num_threads << " threads: " << tm << "s for Rome graph." << endl;
 
+    int ** D = new int*[G.VortexCount + 1];
+    int ** P = new int*[G.VortexCount + 1];
+    for (int i = 0; i < G.VortexCount + 1; i++) {
+	    D[i] = new int [G.VortexCount + 1];
+	    P[i] = new int [G.VortexCount + 1];
+    }
+    Graph G2 = G;
+    G2.VortexCount = G.VortexCount + 1;
+    G2.EdgeCount = G.EdgeCount + G.VortexCount;
+    G2.E = new Edge[G2.EdgeCount];
+    
+    #pragma omp parallel for
+    for (int i = 0; i < G.VortexCount; i++) G2.E[i] = G.E[i];
+    #pragma omp parallel for
+    for (int i = 0; i < G2.VortexCount-1; i++) {
+	    G2.E[G.EdgeCount + i].start = G.VortexCount;
+	    G2.E[G.EdgeCount + i].end = i;
+	    G2.E[G.EdgeCount + i].weight = 0;
+    }	
+    
+    BellmanFord(G2, G.VortexCount, D[G.VortexCount], P[G.VortexCount]); 
+    #pragma omp parallel for
+    for (int i = 0; i < G.EdgeCount; i++) G.E[i].weight -= (D[G.VortexCount][G.E[i].start] - D[G.VortexCount][G.E[i].end]);
+    #pragma omp parallel for
+    for (int i = 0; i < G.VortexCount; i++) Dijkstra(G,i,D[i],P[i]);
+    #pragma omp parallel for
+    for (int i = 0; i < G.EdgeCount; i++) G.E[i].weight += (D[G.VortexCount][G.E[i].start] - D[G.VortexCount][G.E[i].end]);
+    
+    cout << num_threads << " threads: " << omp_get_wtime() - tm << "s for Rome graph." << endl;
+    
+    // Test case
     //for (int i = 0; i < G.VortexCount; i++)
-    //   cout << 0 << "-" << i << " " << D[0][i] << endl;
+    //   cout << 0 << "-" << i << " " << D[0][i] - (D[G.VortexCount][0 - D[i]) << endl;
+    PrintPath(G, 0, 787, D[0][787] - (D[G.VortexCount][0 - D[787]) , P[0]); 
+    PrintPath(G, 0, 147, D[0][147] - (D[G.VortexCount][0 - D[147]) , P[0]); 
 
-    cout << "Path 1-788, d = " << D[0][787] << endl;
-    ConstructPath(P,0,787,path);    
-    for (int i = 0; i < path.size(); i++) cout << path[i] + 1 << "-";
-    cout << endl;
-    int s = 0;
-    for (int i = 0; i < path.size() - 1; i++){
-        cout << G.A[path[i]][path[i+1]] << "+";
-        s += G.A[path[i]][path[i+1]];
+    delete[] G.E;
+    delete[] G2.E;
+    for (int i = 0; i < G.VortexCount + 1; i++) {
+	    delete[] D[i];
+	    delete[] P[i];
     }
-    cout << " = " << s << endl;
-    /*
-    cout << "Path 1-148, d = " << D[0][147] << endl;
-    ConstructPath(P,0,147,path);    
-    for (int i = 0; i < path.size(); i++) cout << path[i] + 1 << "-";
-    cout << endl;
-    s = 0;
-    for (int i = 0; i < path.size() - 1; i++){
-        cout << G.A[path[i]][path[i+1]] << "+";
-        s += G.A[path[i]][path[i+1]];
-    }
-    cout << " = " << s << endl;
-    */
-    for (int i = 0; i < G.VortexCount; i++) {
-        delete[] D[i];
-        delete[] P[i];
-        delete[] G.A[i];
-    }    
-    delete[] D; delete[] P; delete[] G.A;
+    delete[] D;
+    delete[] P;
     return 0;
 }
